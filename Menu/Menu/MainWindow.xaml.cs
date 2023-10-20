f77f1066b89dad8c8165824fa12f6f1477e04a88
andreytkach85 <andreytkach85@gmail.com>
2023-10-20 17:48:59 +0200

    menu

---
+++ Menu/Menu/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Menu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _myProperty;
+
+        public string MyProperty
+        {
+            get { return _myProperty; }
+            set
+            {
+                if (_myProperty != value)
+                {
+                    _myProperty = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyProperty = "This is a status bar!";
+            // Set the DataContext of the Window to itself (or any other object containing the property)
+            DataContext = this;
+        }
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            // Code to handle "Exit" menu item click
+            Application.Current.Shutdown();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
